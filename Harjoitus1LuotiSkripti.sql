496b0564ec3cdd43cb840a95a5bc1020958f3b8b
Mika Vainio <koti.mika.vainio@gmail.com>
2022-09-07 10:25:05 +0300

    päivitetty luotiskriptiä

--- Harjoitus1LuotiSkripti.sql
+++ Harjoitus1LuotiSkripti.sql
@@ -1,35 +1,34 @@
 
-CREATE SEQUENCE public.postin_tyyppi_tyyppikoodi_seq_1;
+CREATE SEQUENCE public.osoitetyyppi_tyyppikoodi_seq_1;
 
-CREATE TABLE public.postin_tyyppi (
-                tyyppikoodi INTEGER NOT NULL DEFAULT nextval('public.postin_tyyppi_tyyppikoodi_seq_1'),
+CREATE TABLE public.osoitetyyppi (
+                tyyppikoodi INTEGER NOT NULL DEFAULT nextval('public.osoitetyyppi_tyyppikoodi_seq_1'),
                 tyyppinimitys VARCHAR(30) NOT NULL,
-                CONSTRAINT postin_tyyppi_pk PRIMARY KEY (tyyppikoodi)
+                CONSTRAINT osoitetyyppi_pk PRIMARY KEY (tyyppikoodi)
 );
 
 
-ALTER SEQUENCE public.postin_tyyppi_tyyppikoodi_seq_1 OWNED BY public.postin_tyyppi.tyyppikoodi;
+ALTER SEQUENCE public.osoitetyyppi_tyyppikoodi_seq_1 OWNED BY public.osoitetyyppi.tyyppikoodi;
 
 CREATE TABLE public.henkilo (
                 opiskelijanumero VARCHAR(8) NOT NULL,
-                etunimi VARCHAR(20) NOT NULL,
+                etunimi VARCHAR(30) NOT NULL,
                 sukunimi VARCHAR(30) NOT NULL,
-                tutkinto VARCHAR(100) DEFAULT Tieto- ja viestintätekniikan perustutkinto NOT NULL,
                 CONSTRAINT henkilo_pk PRIMARY KEY (opiskelijanumero)
 );
 
 
 CREATE TABLE public.sahkoposti (
-                osoite VARCHAR(80) NOT NULL,
+                osoite VARCHAR(100) NOT NULL,
                 opiskelijanumero VARCHAR(8) NOT NULL,
                 tyyppikoodi INTEGER NOT NULL,
                 CONSTRAINT sahkoposti_pk PRIMARY KEY (osoite)
 );
 
 
-ALTER TABLE public.sahkoposti ADD CONSTRAINT postin_tyyppi_sahkoposti_fk
+ALTER TABLE public.sahkoposti ADD CONSTRAINT osoitetyyppi_sahkoposti_fk
 FOREIGN KEY (tyyppikoodi)
-REFERENCES public.postin_tyyppi (tyyppikoodi)
+REFERENCES public.osoitetyyppi (tyyppikoodi)
 ON DELETE NO ACTION
 ON UPDATE NO ACTION
 NOT DEFERRABLE;
